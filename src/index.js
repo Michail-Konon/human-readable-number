b4c8bca9c31691201a1a57146d475a78f42c7bd6
michail-konon <greateyuri@gamil.com>
2023-01-15 22:46:05 +0300

    feat: add new test and 1-19 numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+              'seventeen', 'eighteen', 'nineteen'];
+    const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+              'ninety'];
+
+    //  from 1 to 19
+    if (number < 20) {
+        return units[number];
+    }
+
 }
